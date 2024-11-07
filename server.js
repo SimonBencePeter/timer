b5313cdda6e4f284fdd14b942be8376971f6bb48
SimonBencePeter <152310866+SimonBencePeter@users.noreply.github.com>
2024-11-07 07:56:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,75 +1,84 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const validCredentials = {
-    username: "admin",
-    password: "admin123"
-};
+const SECRET_KEY = "supersecretkey";
+const ADMIN_USER = { username: "admin", password: "password123" };
 
 let countdownTime = 0;
 let countdownInterval;
 
-io.on("connection", (socket) => {
-    console.log("Új kliens csatlakozott");
-    socket.emit("updateCountdown", countdownTime);
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
     
-    // Bejelentkezés kezelése
-    socket.on("authenticate", (credentials) => {
-        if (credentials.username === validCredentials.username && credentials.password === validCredentials.password) {
-            socket.authenticated = true; // Beállítjuk a hitelesítést
-            socket.emit("authenticated");
-        } else {
-            socket.emit("authenticationFailed");
-        }
-    });
+    if (username === ADMIN_USER.username && password === ADMIN_USER.password) {
+        const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+        return res.json({ token });
+    } else {
+        return res.status(401).json({ message: "Hibás felhasználónév vagy jelszó" });
+    }
+});
 
-    // Visszaszámlálás beállítása, ha hitelesítve van
+function authenticateToken(socket, next) {
+    const token = socket.handshake.auth.token;
+    if (!token) return next(new Error("Hitelesítés szükséges"));
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return next(new Error("Érvénytelen token"));
+        socket.user = user;
+        next();
+    });
+}
+
+io.use(authenticateToken);
+
+io.on("connection", (socket) => {
+    socket.emit("updateCountdown", countdownTime);
+
     socket.on("setCountdown", (time) => {
-        if (socket.authenticated) {  // Ellenőrizzük, hogy be van-e jelentkezve
-            countdownTime = time;
-            clearInterval(countdownInterval);
-            io.emit("updateCountdown", countdownTime);
+        countdownTime = time;
+        clearInterval(countdownInterval);
+        io.emit("updateCountdown", countdownTime);
 
-            countdownInterval = setInterval(() => {
-                if (countdownTime > 0) {
-                    countdownTime--;
-                    io.emit("updateCountdown", countdownTime);
-                } else {
-                    clearInterval(countdownInterval);
-                }
-            }, 1000);
-        }
+        countdownInterval = setInterval(() => {
+            if (countdownTime > 0) {
+                countdownTime--;
+                io.emit("updateCountdown", countdownTime);
+            } else {
+                clearInterval(countdownInterval);
+            }
+        }, 1000);
     });
 
-    // Stop és Resume logika
     socket.on("stopCountdown", () => {
-        if (socket.authenticated) {
-            clearInterval(countdownInterval);
-            io.emit("updateCountdown", countdownTime);
-        }
+        clearInterval(countdownInterval);
+        io.emit("updateCountdown", countdownTime);
     });
 
     socket.on("resumeCountdown", () => {
-        if (socket.authenticated) {
-            countdownInterval = setInterval(() => {
-                if (countdownTime > 0) {
-                    countdownTime--;
-                    io.emit("updateCountdown", countdownTime);
-                } else {
-                    clearInterval(countdownInterval);
-                }
-            }, 1000);
-        }
-    });
+        clearInterval(countdownInterval);
+        io.emit("updateCountdown", countdownTime);
 
-    socket.on("disconnect", () => {
-        console.log("Kliens lecsatlakozott");
+        countdownInterval = setInterval(() => {
+            if (countdownTime > 0) {
+                countdownTime--;
+                io.emit("updateCountdown", countdownTime);
+            } else {
+                clearInterval(countdownInterval);
+            }
+        }, 1000);
     });
 });
+
+app.use(express.static(path.join(__dirname)));
 
 server.listen(3000, () => {
     console.log("Szerver fut a 3000-es porton");
